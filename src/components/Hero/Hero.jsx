f13bdba7cc7153bbc13da38f7a86c4201e030f4c
donuthappy <mateo.richard420@gmail.com>
2023-10-23 05:18:23 -0700

    Add text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -126,9 +126,9 @@ const Hero = () => {
                                             <button type="submit">Check</button>
                                         </div>
                                     </form>
-                                </div>
-                                
                                 <p>As an Amazon Associate, commissions are earned from qualifying purchases at no cost to you.</p>
+                                </div>
+
                             </div>
                         </div>
                         <div className="col-2">
